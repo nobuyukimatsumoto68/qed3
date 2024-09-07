80aa35ec62102d12444392d55aab92b0ee6f5ea0
Nobuyuki Matsumoto <mtsmtnbyk@gmail.com>
2024-09-06 21:23:09 -0400

    mac env

--- set.sh
+++ set.sh
@@ -51,11 +51,16 @@ mkdir -p ${DORBIT}
 
 FORBIT=${DORBIT}/q${q}k${k}_orbit.dat
 
-make -C grp gen_grp_o3
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    make -C grp gen_grp_o3
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    cd ${dir_qfe}/grp
+    cp Makefile Makefile.mac
+    sed -i.bak 's/python3/${PYTHON}/g' "Makefile.mac"
+    cd ${dir_qfe}
 
-
-cd grp
-cp Makefile Makefile.mac
-sed -i.bak 's/python3/${PYTHON}/g' "Makefile.mac"
-export PYTHON=/opt/anaconda3/bin/python3
-make gen_grp_o3 -f Makefile.mac
+    export PYTHON=/opt/anaconda3/bin/python3
+    make gen_grp_o3 -C grp -f Makefile.mac
+else
+    echo "unknown OS"
+fi
