7377abdddfd2582e261af07467917951966f3f52
Nobuyuki Matsumoto <mtsmtnbyk@gmail.com>
2024-10-16 15:49:02 -0400

    simpler setup

--- test_dirac.cc
+++ test_dirac.cc
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]){
 
   // geometry
   const int q=5; // icosahedron
-  const int n_refine=2; // no refinement
+  const int n_refine=1; // no refinement
 
   QfeLatticeS2 lattice(q, n_refine);
   Dirac1fonS2 D(lattice, n_refine);
@@ -17,28 +17,28 @@ int main(int argc, char* argv[]){
 
   // for(int ix=0; ix<lattice.n_sites; ix++){
   //   for(int jj=0; jj<lattice.sites[ix].nn; jj++){
-  int ix=0;
-  int jj=0;
-  const int iy = lattice.sites[ix].neighbors[jj];
-  const int il = lattice.sites[ix].links[jj];
+  // int ix=0;
+  // int jj=0;
+  // const int iy = lattice.sites[ix].neighbors[jj];
+  // const int il = lattice.sites[ix].links[jj];
 
-  {
-    auto mat_xy = 0.125*D.ellstar[il] * ( D.r*D.sigma[0] - D.gamma(ix, iy) ) * D.Omega(ix, iy);
-    std::cout << "mat_xy = " << std::endl
-	      << mat_xy << std::endl;
-  }
-  {
-    auto mat_yx = 0.125*D.ellstar[il] * ( D.r*D.sigma[0] - D.gamma(iy, ix) ) * D.Omega(iy, ix);
-    std::cout << "mat_yx = " << std::endl
-	      << mat_yx << std::endl;
-  }
-
-
-  // auto mat = D.matrix_form();
-  // auto ev = mat.eigenvalues();
-  // for(int i=0; i<ev.size(); i++){
-  //   std::cout << ev[i].real() << " " << ev[i].imag() << std::endl;
+  // {
+  //   auto mat_xy = 0.125*D.ellstar[il] * ( D.r*D.sigma[0] - D.gamma(ix, iy) ) * D.Omega(ix, iy);
+  //   std::cout << "mat_xy = " << std::endl
+  // 	      << mat_xy << std::endl;
   // }
+  // {
+  //   auto mat_yx = 0.125*D.ellstar[il] * ( D.r*D.sigma[0] - D.gamma(iy, ix) ) * D.Omega(iy, ix);
+  //   std::cout << "mat_yx = " << std::endl
+  // 	      << mat_yx << std::endl;
+  // }
+
+
+  auto mat = D.matrix_form();
+  auto ev = mat.eigenvalues();
+  for(int i=0; i<ev.size(); i++){
+    std::cout << ev[i].real() << " " << ev[i].imag() << std::endl;
+  }
 
   // ----------------------------------
 
