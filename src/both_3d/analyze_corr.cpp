35a634690a952a621b2a14e4376d25c4c98e08e1
Nobuyuki Matsumoto <mtsmtnbyk@gmail.com>
2025-05-07 15:23:47 -0400

    jk

---
+++ src/both_3d/analyze_corr.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <iomanip>
+#include <filesystem>
+
+#include <cmath>
+
+#include <cassert>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+namespace fs = std::filesystem;
+
+
+struct Corr {
+  std::vector<double> data;
+
+  Corr(){}
+
+  Corr( const std::string& file ){
+    data.clear();
+    readfile( file );
+  }
+
+  std::size_t size() const { return data.size(); }
+
+  void clear() {
+    data.clear();
+  }
+
+  void readfile( const std::string& file ){
+    std::string str;
+    std::ifstream ifs(file);
+    if(!ifs) assert(false);
+
+    while (std::getline(ifs, str)){
+      std::istringstream iss(str);
+      double v1;
+      iss >> v1;
+      data.push_back( v1 );
+    }
+  }
+
+  double operator[](const int i) const { return data[i]; }
+  double& operator[](const int i) { return data[i]; }
+
+  Corr& operator+=(const Corr& rhs){
+    if( data.size()==0 ) {
+      data = rhs.data;
+    }
+    else{
+      assert( data.size()==rhs.data.size() );
+      for(int i=0; i<data.size(); i++) data[i] += rhs.data[i];
+    }
+    return *this;
+  }
+
+  Corr& operator-=(const Corr& rhs){
+    if( data.size()==0 ) {
+      data = rhs.data;
+    }
+    else{
+      assert( data.size()==rhs.data.size() );
+      for(int i=0; i<data.size(); i++) data[i] -= rhs.data[i];
+    }
+    return *this;
+  }
+
+  friend Corr operator-(Corr v, const Corr& w) { v -= w; return v; }
+
+  Corr& operator*=(const Corr& rhs){
+    assert( data.size()==rhs.data.size() );
+    for(int i=0; i<data.size(); i++) data[i] *= rhs[i];
+    return *this;
+  }
+
+  friend Corr operator*(Corr v, const Corr& w) { v *= w; return v; }
+
+  Corr& operator*(const double& rhs){
+    for(int i=0; i<data.size(); i++) data[i] *= rhs;
+    return *this;
+  }
+
+  Corr& operator*=(const double& rhs){
+    for(int i=0; i<data.size(); i++) data[i] *= rhs;
+    return *this;
+  }
+
+  Corr& operator/=(const double& rhs){
+    for(int i=0; i<data.size(); i++) data[i] /= rhs;
+    return *this;
+  }
+};
+
+
+
+template<typename T>
+struct Jackknife {
+  std::vector<T> binavgs;
+  std::vector<T> jackavgs;
+  T est;
+  T var;
+
+  int nbins;
+  const int binsize;
+
+  Jackknife( const int binsize )
+    : binsize(binsize)
+  {}
+
+  void do_it(){
+    nbins = binavgs.size();
+
+    jackavgs.clear();
+    jackavgs.resize( nbins );
+    for(int i=0; i<nbins; i++){
+      for(int j=0; j<nbins; j++){
+        if(i==j) continue;
+        jackavgs[i] += binavgs[j];
+      }
+      jackavgs[i] /= nbins-1.0;
+    }
+
+    est.clear();
+    for(int i=0; i<nbins; i++){
+      est += jackavgs[i];
+    }
+    est /= nbins;
+
+    var.clear();
+    for(int i=0; i<nbins; i++){
+      T diff = jackavgs[i] - est;
+      var += diff * diff;
+    }
+    var /= nbins;
+    var *= nbins-1.0;
+  }
+
+};
+
+
+
+
+int main(int argc, char **argv){
+
+  std::string path = argv[1];
+  std::string header = argv[2];
+  int conf_min = atoi(argv[3]);
+  int conf_max = atoi(argv[4]);
+  int interval = atoi(argv[5]);
+  int Nt = atoi(argv[6]);
+
+  int binsize = atoi(argv[7]);
+  // int Nt = atoi(argv[6]);
+
+  std::cout << "# path = " << path << std::endl;
+  std::cout << "# header = " << header << std::endl;
+  std::cout << "# conf_min = " << conf_min << std::endl;
+  std::cout << "# interval = " << interval << std::endl;
+  std::cout << "# Nt = " << Nt << std::endl;
+  std::cout << "# binsize = " << binsize << std::endl;
+
+  // std::vector<fs::path> files;
+
+  int bincount=0;
+  Corr binned;
+  // std::vector<Corr> binavgs;
+  Jackknife<Corr> jk(binsize);
+
+  for(int conf=conf_min; conf<conf_max; conf+= interval){
+    std::string file = path+"/"+header+std::to_string(conf)+".dat";
+    if( !fs::exists( file ) ) break;
+    Corr corr(file);
+
+    binned += corr;
+    bincount++;
+
+    if(bincount==binsize){
+      binned /= binsize;
+      jk.binavgs.push_back( binned );
+
+      binned.clear();
+      bincount=0;
+    }
+  }
+  jk.do_it();
+
+  for( int i=0; i<jk.est.size(); i++ ){
+    std::cout << jk.est[i] << " " << std::sqrt(jk.var[i]) << std::endl;
+  }
+
+  // Iterate through the directory and add files to the vector
+  // for (const auto& entry : fs::directory_iterator(path)) {
+  //   if (fs::is_regular_file(entry)) {
+  //     files.push_back(entry.path());
+  //   }
+  // }
+
+  // // Sort the files alphabetically
+  // std::sort(files.begin(), files.end());
+
+  // // Print the sorted file names
+  // for (const auto& file : files) {
+  //   std::cout << file.filename().string() << std::endl;
+  // }
+
+  return 0;
+}
---
+++ src/both_3d/test_gauge.sh
@@ -0,0 +1,3 @@
+g++ analyze_corr.cpp -std=c++17 -O3
+
+./a.out beta50.000000at0.069197nt32L1 plaq_ss_t_ 1000 11000 10 32 10
