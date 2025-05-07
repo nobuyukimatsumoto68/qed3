fdfda18ece9b116a7ed28b150243403a45d2673a
Nobuyuki SCC <nmatsum@bu.edu>
2025-05-07 15:24:33 -0400

    recover

---
+++ src/both_3d/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+
+# https://www.bu.edu/tech/support/research/system-usage/running-jobs/submitting-jobs/
+# to submit: qsub script.sh
+
+#------ qsub options --------#
+#$ -P qfe
+#$ -M nmatsum@bu.edu
+##### run time limit. format: hh:mm:ss; default 12 hrs
+#$ -l h_rt=48:00:00
+#$ -l cpu_arch=skylake
+#$ -pe omp 12
+
+
+
+
+
+# --------- job info -----------#
+
+echo "start"
+echo "=========================================================="
+echo "Start date : $(date)"
+echo "Job name : $JOB_NAME"
+echo "Job ID : $JOB_ID  $SGE_TASK_ID"
+echo "NSlots : $NSLOTS"
+echo "Host name : $HOSTNAME"
+echo "working directory : $TMPDIR"
+echo "=========================================================="
+
+#-------------#
+
+module load gcc/13.2.0
+module load cuda/12.5
+
+#------- Program execution -------#
+
+echo "running program"
+pwd
+echo ${app}
+${app} # ${mult} ${binsize}
+echo "finished"
