0726d47347d79e0166b6a3c6aae381ea70a68706
Nobuyuki SCC <nmatsum@bu.edu>
2025-07-23 11:53:33 -0400

    conflict

--- src/both_3d/gauge.cu
+++ src/both_3d/gauge.cu
@@ -58,7 +58,7 @@ namespace Comp{
   constexpr int NPARALLEL=1; // 12
   constexpr int NSTREAMS=12; // for grad loop
 #endif
-  const int kmax=1e2;
+  // const int kmax=1e2;
 
   // gsq = 0.05
   // constexpr int N_REFINE=1;
@@ -85,7 +85,7 @@ namespace Comp{
 
 
   constexpr int N_REFINE=4;
-  // const int kmax=1e8;
+  const int kmax=1e8;
   // constexpr int Nt=24; // 10
   // constexpr int nsteps=52;
   // constexpr int Nt=32; // 10
