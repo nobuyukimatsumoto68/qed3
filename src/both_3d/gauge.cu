778490b437dd86dcc864e08085e07318c20f0dcd
Nobuyuki SCC <nmatsum@bu.edu>
2025-07-26 19:51:16 -0400

    conflict

--- src/both_3d/gauge.cu
+++ src/both_3d/gauge.cu
@@ -92,10 +92,10 @@ namespace Comp{
   // constexpr int nsteps=58;
   // constexpr int Nt=48; // 10
   // constexpr int nsteps=52;
-  // constexpr int Nt=64; // 10
-  // constexpr int nsteps=60;
-  constexpr int Nt=96; // 10
-  constexpr int nsteps=64;
+  constexpr int Nt=64; // 10
+  constexpr int nsteps=62;
+  // constexpr int Nt=96; // 10
+  // constexpr int nsteps=64;
 
 
 
